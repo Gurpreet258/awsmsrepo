1729f317cd6483a7dee0a71a6f80ecac5dc8b977
gurpreet <gurpreet258@gmail.com>
2021-04-08 17:34:00 +0530

    added autoconfig annotation

--- couponservice/src/main/java/com/bharath/springcloud/CouponserviceApplication.java
+++ couponservice/src/main/java/com/bharath/springcloud/CouponserviceApplication.java
@@ -3,11 +3,13 @@ package com.bharath.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAutoConfiguration
 @SpringBootApplication
 public class CouponserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CouponserviceApplication.class, args);
+
 	}
 
 }
