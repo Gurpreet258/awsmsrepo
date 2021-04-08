a7537dee753ee37f2e6dcc8f942c7123a77f62c0
gurpreet <gurpreet258@gmail.com>
2021-04-08 17:34:58 +0530

    Revert "added autoconfig annotation"
    This reverts commit 1729f317cd6483a7dee0a71a6f80ecac5dc8b977.

--- couponservice/src/main/java/com/bharath/springcloud/CouponserviceApplication.java
+++ couponservice/src/main/java/com/bharath/springcloud/CouponserviceApplication.java
@@ -3,13 +3,11 @@ package com.bharath.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableAutoConfiguration
 @SpringBootApplication
 public class CouponserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CouponserviceApplication.class, args);
-
 	}
 
 }
